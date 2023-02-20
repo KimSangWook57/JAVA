322f18b608ace3adea1cefbcbfe2462081f6ef69
KimSangWook57 <122770754+KimSangWook57@users.noreply.github.com>
2023-02-20 17:57:48 +0900

    8퀸 문제 해결 코드 찾기.

--- chap05_recursive/EightQueen.java
+++ chap05_recursive/EightQueen.java
@@ -3,28 +3,33 @@ package chap05_recursive;
 import java.awt.Point;
 
 public class EightQueen {
+	//  문제 해결형 자바 코딩 실습이 필요.
+	//  - main()의 작성 방법 훈련 -
+	
+	//  배열 초기화
+	int[][] data = new int[8][8];
 
-//  문제 해결형 자바 코딩 실습이 필요.
-//  - main()의 작성 방법 훈련 - 
 	public static void main(String[] args) {
-		// 배열 초기화
-		int[][] data = new int[8][8];
+
+		// 문제 해결 코드.
+		Mystack s1 = new Mystack();
+
 		// 배열의 열 출력: 8개의 queen 배치.(92개의 해를 구해 보여주기)
-		SolveQueen(data);
+		SolveQueen();
 	}
 
 //  함수 호출로 구현 훈련. 0과 1을 빈 공간과 채워진 공간으로 바꾸어 준 뒤,
 //  결과값을 출력해 준다.
-	static void SolveQueen(int[][] d) {
-		for (int i = 0; i < d.length; i++) {
-			int[] d2 = d[i];
-			for (int j = 0; j < d2.length; j++) {
-				System.out.print(d2[j] + " ");
+	static void SolveQueen() {
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				System.out.print(data[row][col] + " ");
 			}
 			System.out.println();
 		}
 
 	}
+
 }
 //  => 8 Queen 문제 풀기: 스택 사용하여 backtracking 코딩 실습.
 //  -> 2차원 배열에 0으로 초기화, Queen을 배치하면 1로 설정,
@@ -32,36 +37,72 @@ public class EightQueen {
 //  -> stack에 Point 객체를 생성하여 push, pop::(x, y, move) =>
 //  move는 다음 이동 가능 candidate의 column임.
 
-class MyStack {
-	int top;
-	Point[] data;
-
-	void Push(Point p) {
-		// 1을 집어넣는 코드?
-	}
-
-	Point Pop() {
-		// 조건이 맞지 않는 곳에 1이 들어가지 못하게 하는 코드?
-	}
-
 	// current row에 대한 다음 배치 가능한 모든 column을 조사하고 move[]에 1로 설정.
-	void NextMove(int[] move, int row) {
-		// 배치가 가능할 때만 move[]에 1을 넣는다.
+	// 조건이 맞는 곳에 퀸을 집어넣는다.
+	void NextMove(Point move, int value) {
+		// 배치가 가능할 때만 move에 1을 넣는다.
+		data[move.x][move.y] = value;
 	}
 
-	// currentRow에 대하여 queen을 (x,y)에 배치 가능하면 true.
-	boolean CheckMove(int currentRow, int row, int col) {
-		
-	}
-	
+	class MyStack {
+		// int top;
+		Point[] data;
+
+		// 문제를 푸는 코드.
+		void solve(int row) {
+			// 모든 row를 돌았다면, 해결방법을 반환한다.
+			if (row == 8) {
+				return;
+			}
+
+			for (int col = 0; col < 8; col++) {
+				// Push값이 비었다면, 퀸을 새로운 포지션에 놓는다.
+				if (CheckMove(new Point(row, col))) {
+					Point queenPos = new Point(row, col);
+					NextMove(queenPos, 1);
+					
+					// 다음 칸으로 이동
+					solve(row + 1);
+					
+					// 해결법을 찾지 못했다면, 퀸을 제거한다.
+					if (!)
+					
+				}
+			}
+
+		}
+
+//		void Push(Point p) {
+//
+//		}
+
+		Point Pop() {
+			// 조건이 맞지 않으면 다시 처음으로 돌아간다.
+		}
+
+		// currentRow에 대하여 queen을 (x,y)에 배치 가능하면 true.
+		boolean CheckMove(int currentRow, int row, int col) {
+
+			return true;
+		}
+
 //  => backtracking을 stack을 이용하여 push/pop으로 해결하는 것을 보이는 것임
 
 //  -> 가로, 세로, 대각선에 대한 충돌 체크 함수 코딩
 	boolean CheckMove(x, y) {
-	checkRow(x);		// 가로 체크		
-	checkCol(y);		// 세로 체크
-	checkDiagSW(x,y);   // 오른쪽 위에서 왼쪽 아래로	x++, y-- or x--, y++ where 0<= x,y <= 7 
-	checkDiagSE(x,y);   // 왼쪽 위에서 오른쪽 아래로	x++, y++ or x--, y--
+		checkRow(x);		// 가로 체크		
+		checkCol(y);		// 세로 체크
+		checkDiagSW(x,y);   // 오른쪽 위에서 왼쪽 아래로	x++, y-- or x--, y++ where 0<= x,y <= 7 
+		checkDiagSE(x,y);   // 왼쪽 위에서 오른쪽 아래로	x++, y++ or x--, y--
     }
+
+	boolean checkRow(int row) {
+		for (int j = 0; j < data.length(); j++) {
+			if (row == ) {
+				return false;
+			}
+		return true;
+		}
+	}
 
 }
---
+++ chap05_recursive/EightQueenProblem.java
@@ -0,0 +1,69 @@
+package chap05_recursive;
+
+import java.awt.Point;
+
+public class EightQueenProblem {
+    
+    // Define the size of the chessboard
+    private static final int BOARD_SIZE = 8;
+    
+    // Store the positions of the queens on the chessboard
+    private static Point[] queens = new Point[BOARD_SIZE];
+    
+    public static void main(String[] args) {
+        // Try to place the queens and print the solution if successful
+        boolean success = placeQueens(0);
+        if (success) {
+            printBoard();
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+    
+    // Recursive backtracking algorithm to find a valid solution
+    private static boolean placeQueens(int row) {
+        // If all rows have been filled with queens, a solution has been found
+        if (row == BOARD_SIZE) {
+            return true;
+        }
+        // Try placing the queen in each column of the current row
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            Point point = new Point(col, row);
+            if (isValid(point)) { // Check if this position is valid
+                queens[row] = point; // Place the queen at this position
+                if (placeQueens(row+1)) { // Recursively move to the next row
+                    return true; // A valid solution was found
+                }
+            }
+        }
+        return false; // No valid solution was found for this row
+    }
+    
+    // Check whether a potential queen position conflicts with previous queens
+    private static boolean isValid(Point point) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            Point queen = queens[i];
+            if (queen != null && (queen.x == point.x || queen.y == point.y || 
+                    queen.x - queen.y == point.x - point.y ||
+                    queen.x + queen.y == point.x + point.y)) {
+                // The new queen would be in conflict with a previously placed queen
+                return false;
+            }
+        }
+        return true; // The new queen position is valid
+    }
+    
+    // Print out the solution as a simple ASCII chessboard
+    private static void printBoard() {
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                if (queens[row] != null && queens[row].x == col) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("- ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
