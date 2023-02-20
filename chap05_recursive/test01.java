27b255cfda68318d21c5d987ecd2f01266526b52
KimSangWook57 <122770754+KimSangWook57@users.noreply.github.com>
2023-02-20 23:00:38 +0900

    참고용 코드 수정.

--- chap05_recursive/test01.java
+++ chap05_recursive/test01.java
@@ -4,66 +4,112 @@ import java.awt.Point;
 
 public class test01 {
     
-    // Define the size of the chessboard
-    private static final int BOARD_SIZE = 8;
-    
-    // Store the positions of the queens on the chessboard
-    private static Point[] queens = new Point[BOARD_SIZE];
-    
-    public static void main(String[] args) {
-        // Try to place the queens and print the solution if successful
-        boolean success = placeQueens(0);
-        if (success) {
-            printBoard();
-        } else {
-            System.out.println("No solution found.");
-        }
-    }
-    
-    // Recursive backtracking algorithm to find a valid solution
-    private static boolean placeQueens(int row) {
-        // If all rows have been filled with queens, a solution has been found
-        if (row == BOARD_SIZE) {
-            return true;
-        }
-        // Try placing the queen in each column of the current row
-        for (int col = 0; col < BOARD_SIZE; col++) {
-            Point point = new Point(col, row);
-            if (isValid(point)) { // Check if this position is valid
-                queens[row] = point; // Place the queen at this position
-                if (placeQueens(row+1)) { // Recursively move to the next row
-                    return true; // A valid solution was found
-                }
-            }
-        }
-        return false; // No valid solution was found for this row
-    }
-    
-    // Check whether a potential queen position conflicts with previous queens
-    private static boolean isValid(Point point) {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            Point queen = queens[i];
-            if (queen != null && (queen.x == point.x || queen.y == point.y || 
-                    queen.x - queen.y == point.x - point.y ||
-                    queen.x + queen.y == point.x + point.y)) {
-                // The new queen would be in conflict with a previously placed queen
-                return false;
-            }
-        }
-        return true; // The new queen position is valid
-    }
-    
-    // Print out the solution as a simple ASCII chessboard
-    private static void printBoard() {
-        for (int row = 0; row < BOARD_SIZE; row++) {
-            for (int col = 0; col < BOARD_SIZE; col++) {
-                if (queens[row] != null && queens[row].x == col) {
-                    System.out.print("Q ");
-                } else {
-                    System.out.print("- ");
-                }
-            }
-            System.out.println();
-        }
-    }
+	/* Java program to solve N Queen Problem using
+	   backtracking */
+	public class NQueenProblem {
+	    final int N = 4;
+	 
+	    /* A utility function to print solution */
+	    void printSolution(int board[][])
+	    {
+	        for (int i = 0; i < N; i++) {
+	            for (int j = 0; j < N; j++)
+	                System.out.print(" " + board[i][j]
+	                                 + " ");
+	            System.out.println();
+	        }
+	    }
+	 
+	    /* A utility function to check if a queen can
+	       be placed on board[row][col]. Note that this
+	       function is called when "col" queens are already
+	       placeed in columns from 0 to col -1. So we need
+	       to check only left side for attacking queens */
+	    boolean isSafe(int board[][], int row, int col)
+	    {
+	        int i, j;
+	 
+	        /* Check this row on left side */
+	        for (i = 0; i < col; i++)
+	            if (board[row][i] == 1)
+	                return false;
+	 
+	        /* Check upper diagonal on left side */
+	        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+	            if (board[i][j] == 1)
+	                return false;
+	 
+	        /* Check lower diagonal on left side */
+	        for (i = row, j = col; j >= 0 && i < N; i++, j--)
+	            if (board[i][j] == 1)
+	                return false;
+	 
+	        return true;
+	    }
+	 
+	    /* A recursive utility function to solve N
+	       Queen problem */
+	    boolean solveNQUtil(int board[][], int col)
+	    {
+	        /* base case: If all queens are placed
+	           then return true */
+	        if (col >= N)
+	            return true;
+	 
+	        /* Consider this column and try placing
+	           this queen in all rows one by one */
+	        for (int i = 0; i < N; i++) {
+	            /* Check if the queen can be placed on
+	               board[i][col] */
+	            if (isSafe(board, i, col)) {
+	                /* Place this queen in board[i][col] */
+	                board[i][col] = 1;
+	 
+	                /* recur to place rest of the queens */
+	                if (solveNQUtil(board, col + 1) == true)
+	                    return true;
+	 
+	                /* If placing queen in board[i][col]
+	                   doesn't lead to a solution then
+	                   remove queen from board[i][col] */
+	                board[i][col] = 0; // BACKTRACK
+	            }
+	        }
+	 
+	        /* If the queen can not be placed in any row in
+	           this column col, then return false */
+	        return false;
+	    }
+	 
+	    /* This function solves the N Queen problem using
+	       Backtracking.  It mainly uses solveNQUtil () to
+	       solve the problem. It returns false if queens
+	       cannot be placed, otherwise, return true and
+	       prints placement of queens in the form of 1s.
+	       Please note that there may be more than one
+	       solutions, this function prints one of the
+	       feasible solutions.*/
+	    boolean solveNQ()
+	    {
+	        int board[][] = { { 0, 0, 0, 0 },
+	                          { 0, 0, 0, 0 },
+	                          { 0, 0, 0, 0 },
+	                          { 0, 0, 0, 0 } };
+	 
+	        if (solveNQUtil(board, 0) == false) {
+	            System.out.print("Solution does not exist");
+	            return false;
+	        }
+	 
+	        printSolution(board);
+	        return true;
+	    }
+	 
+	    // driver program to test above function
+	    public static void main(String args[])
+	    {
+	        NQueenProblem Queen = new NQueenProblem();
+	        Queen.solveNQ();
+	    }
+	}
 }
